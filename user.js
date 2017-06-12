3ddf3c5a7b930acd0725b146235b6bc369f17f81
rytmt <ryoto0114@gmail.com>
2017-06-12 15:52:13 +0900

    misc

---
+++ user.js
@@ -0,0 +1,4 @@
+user_pref("gfx.font_rendering.cleartype_params.rendering_mode", 5);
+user_pref("browser.fullscreen.autohide", false);
+user_pref("browser.tabs.animate", false);
+user_pref("browser.fullscreen.animate", false);
