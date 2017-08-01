9017460b600efe883cac79ef7c3ab18bcaabe0f4
rytmt <rytmt@nxdomain.local>
2017-08-02 00:13:08 +0900

    misc

--- fedora/.emacs.d/custom.el
+++ fedora/.emacs.d/custom.el
@@ -1,10 +1,18 @@
 ;; faces
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(default ((t (:foreground "brightwhite"))))
  '(linum ((t (:background "color-235" :foreground "color-245")))))
 
 ;; packages
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
     (undohist gruvbox-theme undo-tree solarized-theme migemo madhat2r-theme anything))))
