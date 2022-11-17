4f71a150d64ae6f340c211632cf0c18485d52474
rytmt <rytmt@nxdomain.local>
2022-11-17 10:37:24 +0900

    misc

--- wsl2/ubuntu/bin/ical2txt.sh
+++ wsl2/ubuntu/bin/ical2txt.sh
@@ -3,10 +3,17 @@
 ICAL="$1"
 
 DATE_START="$(grep -E '^DTSTART;TZID=' "${ICAL}" | awk -F ':' '{ print $NF }' | cut -c 1-13 | sed 's/T/ /g')"
+YMD_START="$(echo ${DATE_START} | cut -c 1-8)"
 DATE_START="$(date --date="${DATE_START}" "+%Y/%m/%d(%a) %H:%M")"
 
 DATE_END="$(grep -E '^DTEND;TZID=' "${ICAL}" | awk -F ':' '{ print $NF }' | cut -c 1-13 | sed 's/T/ /g')"
-DATE_END="$(date --date="${DATE_END}" "+%Y/%m/%d(%a) %H:%M")"
+YMD_END="$(echo ${DATE_END} | cut -c 1-8)"
+
+if [ "${YMD_START}" = "${YMD_END}" ]; then
+    DATE_END="$(date --date="${DATE_END}" "+%Y/%m/%d(%a) %H:%M" | cut -d ' ' -f 2)"
+else
+    DATE_END="$(date --date="${DATE_END}" "+%Y/%m/%d(%a) %H:%M")"
+fi
 
 ORGANIZER_ADDR="$(grep -E '^ORGANIZER;CN=' "${ICAL}" | awk -F ':' '{ print $NF }' | tr -d '\r')"
 ORGANIZER_NAME="$(grep -E '^ORGANIZER;CN=' "${ICAL}" | cut -d ';' -f 2 | cut -d ':' -f 1 | cut -d '=' -f 2 | tr -d '\r')"
@@ -17,7 +24,7 @@ ROOM="$(grep -E '^LOCATION' "${ICAL}" | awk -F ':' '{ print $NF }' | tr -d '\r')
 
 echo
 echo "作成者: ${ORGANIZER_NAME} (${ORGANIZER_ADDR})"
-echo "  日時: ${DATE_START} 〜 ${DATE_END}"
+echo "  日時: ${DATE_START} - ${DATE_END}"
 echo "  場所: ${ROOM}"
 echo "出席者:"
 echo "${ATTENDEE}" | while read line; do echo "  - ${line}"; done
