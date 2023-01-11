19691e528d54bb5f261bf398bc840a52d0b2a9a4
rytmt <rytmt@nxdomain.local>
2023-01-11 19:28:59 +0900

    misc

--- wsl2/ubuntu/bin/ical2txt.sh
+++ wsl2/ubuntu/bin/ical2txt.sh
@@ -18,19 +18,22 @@ fi
 ORGANIZER_ADDR="$(grep -E '^ORGANIZER;CN=' "${ICAL}" | awk -F ':' '{ print $NF }' | tr -d '\r')"
 ORGANIZER_NAME="$(grep -E '^ORGANIZER;CN=' "${ICAL}" | cut -d ';' -f 2 | cut -d ':' -f 1 | cut -d '=' -f 2 | tr -d '\r')"
 
-ATTENDEE="$(grep -E -A 2 '^ATTENDEE' "${ICAL}" | grep -E -v '^ATTENDEE' | awk -F ':' '{ print $NF }' | grep -F '@' | tr -d '\r')"
+ATTENDEE_REQ="$(grep -E -A 2 '^ATTENDEE;ROLE=REQ' "${ICAL}" | grep -E -v '^ATTENDEE' | awk -F ':' '{ print $NF }' | grep -F '@' | tr -d '\r')"
+ATTENDEE_OPT="$(grep -E -A 2 '^ATTENDEE;ROLE=OPT' "${ICAL}" | grep -E -v '^ATTENDEE' | awk -F ':' '{ print $NF }' | grep -F '@' | tr -d '\r')"
 
 ROOM="$(grep -E '^LOCATION' "${ICAL}" | awk -F ':' '{ print $NF }' | tr -d '\r')"
 
 DESCRIPTION="$(cat "${ICAL}" | awk '/^DESCRIPTION;LANGUAGE=/,/^RRULE:FREQ/ {print $0}' | sed -r -e 's/\r//g' -e 's/DESCRIPTION;LANGUAGE=.*://' | grep -v '^RRULE:FREQ')"
 
 echo
-echo "作成者: ${ORGANIZER_NAME} (${ORGANIZER_ADDR})"
-echo "  日時: ${DATE_START} - ${DATE_END}"
-echo "  場所: ${ROOM}"
-echo "出席者:"
-echo "${ATTENDEE}" | while read line; do echo "  - ${line}"; done
-echo "  - ${ORGANIZER_ADDR}"
+echo "    作成者: ${ORGANIZER_NAME} (${ORGANIZER_ADDR})"
+echo "      日時: ${DATE_START} - ${DATE_END}"
+echo "      場所: ${ROOM}"
+echo "必須出席者:"
+[ "${ATTENDEE_REQ}x" = "x" ] || echo "${ATTENDEE_REQ}" | while read line; do echo "      - ${line}"; done
+echo "      - ${ORGANIZER_ADDR}"
+echo "任意出席者:"
+[ "${ATTENDEE_OPT}x" = "x" ] || echo "${ATTENDEE_OPT}" | while read line; do echo "      - ${line}"; done
 echo
 echo "${DESCRIPTION}"
 
