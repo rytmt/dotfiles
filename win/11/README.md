ed19373d4039f68187416b0b066ad3764a8aa84c
rytmt <rytmt@nxdomain.local>
2023-07-21 21:14:16 +0900

    add win/11

---
+++ win/11/README.md
@@ -0,0 +1,86 @@
+# windows セットアップ
+
+## 初めにやる
+- 「設定」アプリから色々変更する (マウスカーソルとか)
+- タスクバーから邪魔なものを消す
+- ダークモードに設定
+- エクスプローラで拡張子を表示するようにする
+- タスクスケジューラから邪魔なものを消す
+
+## フォントインストール
+- https://github.com/yuru7/HackGen/releases/latest
+  - windows terminal には HackGenNerd Console を適用 (exaのアイコン対応)
+  - vscode には HackGen を適用 (設定同期で自動適用されるはず)
+
+## ソフトウェアインストール
+### 特に設定のいらないもの
+- 7zip
+- ctrl2cap
+- vlc
+- honeyview
+- adobe acrobat reader
+
+### 設定が必要なもの
+#### google日本語入力
+1. タスクバーのアイコンを右クリックし、プロパティを開く
+2. 「一般」タブの「キー設定の選択」から「編集」を選択
+3. 変換キーで IME を有効化、無変換キーで IME を無効化するようにする (半角/全角キーの設定を参考)
+
+#### google chrome
+1. 設定同期 (4114)
+2. ブックマーク等は同期しないようにする
+
+#### keyhac
+1. keyhac のインストール
+2. keyhac の起動
+3. zsh(wsl2) から keyhac_git2local コマンドを実行 (config.py をローカルにコピー)
+4. keyhac 設定のリロード
+
+#### tweeten
+1. GUI から適当に設定
+
+#### clibor
+1. GUI から適当に設定
+
+#### tascher
+1. GUI から適当に設定
+
+#### wsl2
+1. wsl2 の有効化、ディストリビューションのインストール (やり方は都度調べる)
+
+#### windows terminal
+1. microsoft store からインストール
+2. ~/dotfiles/win/10/winterm_colorthemes.json を windows terminal の設定(.json)に追記
+3. GUI から適当に設定
+
+#### vscode
+1. インストール
+2. 設定同期 (rytmt@outlook.jp)
+
+#### old
+##### autohotkey
+1. autohotkey のインストール
+2. dotfiles/win/10/myscript.ahk をダウンロードして、実行
+
+##### tablacus Explorer
+1. インストール
+2. アドオンインストール (ツール -> アドオン)
+   1. ダークモード
+   2. 分割
+   3. 6分割
+   4. フォント設定
+      1. 以下の設定にすると更新日時列の自動調整がうまくいく
+         1. フォント：cica
+         2. サイズ：15
+   5. フォルダ設定
+      1. 「フォルダの表示設定を覚える」と競合するので、これは無効化しておく
+      2. 以下の設定を行う (列幅の自動調整用)
+         1. フィルタ：*
+         2. タイプ：JScript
+         3. オプション
+```
+FV.CurrentViewMode(4,16);
+FV.Columns='"名前" -2 "更新日時" -2 "サイズ" -2 "種類" -2';
+FV.GroupBy='System.Null';
+FV.SortColumn='名前';
+```
---
+++ win/11/config.py
@@ -0,0 +1,100 @@
+﻿import sys
+import os
+import datetime
+
+import pyauto
+from keyhac import *
+
+
+def configure(keymap):
+
+    # --------------------------------------------------------------------
+    # Text editer setting for editting config.py file
+
+    # 設定ファイル用のエディタ指定 (使用しない)
+    # wsl2 の .zshrc に keyhac_edit コマンドが定義してあるのでそちらを使う
+    if 1:
+        keymap.editor = "notepad.exe"
+
+    # --------------------------------------------------------------------
+    # Customizing the display
+
+    # Font
+    keymap.setFont( "MS Gothic", 12 )
+
+    # Theme
+    keymap.setTheme("black")
+
+    # --------------------------------------------------------------------
+    # 関数定義
+
+    # IME 無効化と Escape キーの送信
+    def ime_off_esc():
+        keymap.wnd.setImeStatus(0)
+        keymap.InputKeyCommand("Esc")()
+
+    # IME 無効化/有効化
+    def ime_off():
+        keymap.wnd.setImeStatus(0)
+    def ime_on():
+        keymap.wnd.setImeStatus(1)
+
+    # カーソル移動用
+    input_down_command = keymap.InputKeyCommand("Down")
+    input_up_command = keymap.InputKeyCommand("Up")
+    def down_multi():
+        for i in range(10):
+            input_down_command()
+    def up_multi():
+        for i in range(10):
+            input_up_command()
+
+    # --------------------------------------------------------------------
+    # グローバルキーマップ
+    if 1:
+        keymap_global = keymap.defineWindowKeymap()
+
+        # vimライクなキーマップ
+        keymap_global["C-H"] = "Left"
+        keymap_global["C-J"] = "Down"
+        keymap_global["C-K"] = "Up"
+        keymap_global["C-L"] = "Right"
+        keymap_global["C-A"] = "Home"
+        keymap_global["C-E"] = "End"
+        keymap_global["A-J"] = lambda: down_multi()
+        keymap_global["A-K"] = lambda: up_multi()
+
+        # ウインドウのアクティブ化
+        keymap_global[ "C-1" ] = "W-1"
+        keymap_global[ "C-2" ] = "W-2"
+        keymap_global[ "C-3" ] = "W-3"
+        keymap_global[ "C-4" ] = "W-4"
+        keymap_global[ "C-5" ] = "W-5"
+
+        # ウインドウの移動
+        keymap_global[ "C-8" ] = "W-S-Left"
+        keymap_global[ "C-9" ] = "W-S-Right"
+
+        # IME Off and Escape
+        keymap_global["C-Colon"] = lambda: ime_off_esc()
+
+        # 変換キー/無変換キーでの IME 切り替え
+        keymap_global["(28)"] = lambda: ime_on()
+        keymap_global["(29)"] = lambda: ime_off()
+        # Shift+無変換 で全角英数入力にならないようにする
+        keymap_global["S-(29)"] = lambda: None
+
+
+    # Edit class 用のキーマップ
+    if 1:
+        keymap_edit = keymap.defineWindowKeymap( class_name="Edit" )
+
+        keymap_edit["C-S-D"] = "Home", "S-End", "C-X"
+
+    # Google Chrome用のキーマップ
+    if 1:
+        keymap_chrome = keymap.defineWindowKeymap( exe_name="chrome.exe" )
+
+        keymap_chrome["C-N"] = "C-Tab"
+        keymap_chrome["C-P"] = "C-S-Tab"
+
