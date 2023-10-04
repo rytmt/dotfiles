d8303ee674dbb48cb59f64c212d30c1c6afa8356
rytmt <rytmt@nxdomain.local>
2023-10-04 11:58:53 +0900

    misc

--- wsl2/ubuntu/bin/sum.sh
+++ wsl2/ubuntu/bin/sum.sh
@@ -1,17 +1,3 @@
 #!/bin/sh
 
-sum=0
-while read line; do
-
-    if echo -n "${line}" | grep -Fq '.'; then
-        # float
-        tmp="${line}"
-    else
-        # int
-        tmp="$(echo -n $line | sed 's/^0\+//g')"
-    fi
-
-    sum=$(echo "${sum} + ${tmp}" | bc)
-
-done
-echo "${sum}"
+cat | awk '{sum+=$0} END {print sum}'
