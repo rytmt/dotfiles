fb84b294caa803bad6bdaa4088939773518046aa
rytmt <rytmt@nxdomain.local>
2023-12-04 11:51:55 +0900

    misc

--- wsl2/ubuntu/.zsh/.p10k.zsh
+++ wsl2/ubuntu/.zsh/.p10k.zsh
@@ -213,7 +213,7 @@
 
   ##################################[ dir: current directory ]##################################
   # Default current directory color.
-  typeset -g POWERLEVEL9K_DIR_FOREGROUND=30
+  typeset -g POWERLEVEL9K_DIR_FOREGROUND=60
   # If directory is too long, shorten some of its segments to the shortest possible unique
   # prefix. The shortened directory can be tab-completed to the original.
   typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
