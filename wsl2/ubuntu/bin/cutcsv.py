253481d44fe4b9b6febda83a3cebdfb7c4f823a5
rytmt <rytmt@nxdomain.local>
2024-03-05 10:57:12 +0900

    misc

--- wsl2/ubuntu/bin/cutcsv.py
+++ wsl2/ubuntu/bin/cutcsv.py
@@ -9,28 +9,32 @@ import csv
 # 引数には取り出す対象の列の数値が指定される想定
 args = sys.argv
 
-# 標準入力を一行ずつ読み込む
-for line in sys.stdin:
-    f = io.StringIO()
-    f.write(line)
-    f.seek(0)
+try:
+    # 標準入力を一行ずつ読み込む
+    for line in sys.stdin:
+        f = io.StringIO()
+        f.write(line)
+        f.seek(0)
 
-    # 標準入力をcsv形式として扱い一行読み込む
-    for row in csv.reader(f):
-        # 引数の個数だけループする
-        for i in range(len(args)-1):
-            # ループの終わりの場合は末尾にカンマを付けない
-            if i == len(args)-2:
-                separator = ''
-            else:
-                separator = ','
-            # 引数で指定された列の値をvalueに代入
-            value=row[int(args[i+1])-1]
-            # value にカンマが含まれる場合には、ダブルクォートを付与する
-            if ',' in value:
-                print(f'"{value}"{separator}', end='')
-            else:
-                print(f'{value}{separator}', end='')
+        # 標準入力をcsv形式として扱い一行読み込む
+        for row in csv.reader(f):
+            # 引数の個数だけループする
+            for i in range(len(args)-1):
+                # ループの終わりの場合は末尾にカンマを付けない
+                if i == len(args)-2:
+                    separator = ''
+                else:
+                    separator = ','
+                # 引数で指定された列の値をvalueに代入
+                value=row[int(args[i+1])-1]
+                # value にカンマが含まれる場合には、ダブルクォートを付与する
+                if ',' in value:
+                    print(f'"{value}"{separator}', end='')
+                else:
+                    print(f'{value}{separator}', end='')
 
-        # 引数で指定された列の出力を終えたら改行する
-        print(f'')
+            # 引数で指定された列の出力を終えたら改行する
+            print(f'')
+# | head コマンドなどで標準入力が閉じられてしまった場合
+except BrokenPipeError as e:
+    sys.exit(1)
